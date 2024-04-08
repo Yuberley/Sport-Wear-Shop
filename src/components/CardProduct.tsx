f45bdf62a00886ce20bdb5e3d2421a35b1c6fc35
Yuberley <yuberleyguerrero1999@gmail.com>
2024-04-08 13:08:00 -0500

    product card modification

--- src/components/CardProduct.tsx
+++ src/components/CardProduct.tsx
@@ -12,60 +12,67 @@ interface CardProductProps {
 const CardProduct = ({ product }: CardProductProps) => {
 
     return (
-        <div key={product.id} className="group relative">
-            {product.discount && (
-                <div className="absolute top-0 right-0 bg-green-500 text-white font-bold px-2 py-1 rounded-bl-md border-b-2 border-green-600 rounded-tr-md
-                z-10 transform -translate-x-0 -translate-y-0 rotate-0 group-hover:opacity-75 group-hover:translate-x-2 group-hover:right-3 transition-all duration-300 ease-in-out group-hover:scale-110 hover:rotate-0">
-                    {product.discount}
-                </div>
-            )}
-            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80 ">
-            <Image
-                priority
-                src={product.imagesSrc[0]}
-                alt={product.name}
-                width={550}
-                height={550}
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-            />
-            </div>
-            <div className="mt-4 flex justify-between">
-            <div>
-                <h3 className="text-sm text-gray-700">
-                <Link 
-                    href={{
-                        pathname: '/product',
-                        query: { 
-                            name: replaceSpacesWithDashes(product.name),
-                            id: product.id 
-                        }
-                    }}
-                    >
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    {product.name}
-                </Link>
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">
-                    {
-                        product.colors.map((color, index) => (
-                            <span key={index} className="mr-1">
-                                {color}
-                                {index < product.colors.length - 1 ? ', ' : ''}
-                            </span>
-                        ))
-                    }
-                </p>
-            </div>
-            {/* <p className="text-sm font-medium text-gray-900">{product.price + ' COP'}</p> */}
-            <p className="text-sm font-medium text-gray-900">
-                {product.discount ? 
-                    <span className="line-through text-gray-500">{product.price + ' COP'}</span>
-                    : product.price + ' COP'
+        <Link
+            href={{
+                pathname: '/product',
+                query: { 
+                    name: replaceSpacesWithDashes(product.name),
+                    id: product.id 
                 }
-            </p>
-            </div>
+            }}
+            >
+            <div key={product.id} className="group relative">
+                {product.discount && (
+                    <div className="absolute top-0 right-0 bg-green-500 text-white font-bold px-2 py-1 rounded-bl-md border-b-2 border-green-600 rounded-tr-md
+                    z-10 transform -translate-x-0 -translate-y-0 rotate-0 group-hover:opacity-75 group-hover:translate-x-2 group-hover:right-3 transition-all duration-300 ease-in-out group-hover:scale-110 hover:rotate-0">
+                        {product.discount}
+                    </div>
+                )}
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80 ">
+                <Image
+                    priority
+                    src={product.imagesSrc[0]}
+                    alt={product.name}
+                    width={550}
+                    height={550}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+                </div>
+                <div className="mt-4">
+                    
+                        <div className="flex justify-between">
+                            <h3 className="text-sm text-gray-800 max-w-[180px]">
+                                    {/* <span aria-hidden="true" className="absolute inset-0" /> */}
+                                    {product.name}
+                            </h3>
+                            <p className="text-sm font-medium text-gray-900">
+                                {product.discount ? 
+                                    <div className="flex flex-col">
+                                        <span className=" text-green-600 font-bold">{product.newPrice + ' COP'}</span>
+                                        <span className="line-through text-gray-400 font-normal text-[12px] flex justify-end mb-[-4px]">{product.price + ' COP'}</span>
+                                    </div>
+                                    : 
+                                    <span className="text-gray-600 font-semibold">{product.price + ' COP'}</span>
+                                }
+                            </p>
+                        </div>
+                    <p className="mt-1 text-sm text-gray-400">
+                        {/* {
+                            product.colors.map((color, index) => (
+                                <span key={index} className="mr-1">
+                                    {color}
+                                    {index < product.colors.length - 1 ? ', ' : ''}
+                                </span>
+                            ))
+                        } */}
 
-        </div>
+                                <span className="mr-1">
+                                    {product.description.length > 80 ? product.description.substring(0, 80) + '...' : product.description}
+                                </span>
+                    </p>
+                </div>
+            </div>
+        </Link>
     )
 }
 export default CardProduct;
