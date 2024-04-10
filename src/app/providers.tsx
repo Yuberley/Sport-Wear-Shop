c0ac0545d2998353cb0a3c9b6a7e201e8cfd0a53
Yuberley <yuberleyguerrero1999@gmail.com>
2024-04-10 00:22:00 -0500

    implementing login

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -11,7 +11,7 @@ export function Providers({children}: { children: React.ReactNode }) {
 
     return (
         <NextUIProvider>
-            {pathname !== "/dashboard" && <Header />}
+            {(pathname !== "/dashboard" && pathname !== "/login") && <Header />}
             {children}
         </NextUIProvider>
     )
