81e2ae96bb38e6ad60d3fa722e1c9ea2cc16068c
Yuberley <yuberleyguerrero1999@gmail.com>
2025-03-04 23:59:14 -0500

    rename domain

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,4 @@ export const MAX_SIZE_IMAGE_IN_MB = 1;
 
 export const NAME_BUCKET_IMAGES = 'images';
 
-export const BASE_URL_THIS_WEBSITE = 'https://ylsport.me';
+export const BASE_URL_THIS_WEBSITE = 'https://ylsport.store';
