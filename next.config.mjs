97009f3c374be69824a1df045771a8d7a2829b21
Yuberley <yuberleyguerrero1999@gmail.com>
2025-03-05 00:20:15 -0500

    restore NextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "ycccqacoxogaexuxrjkm.supabase.co",
-        pathname: "/storage/v1/object/public/images/**",
-      },
-    ],
-  },  
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: '**',
+            port: '',
+            pathname: '/**',
+          },
+        ],
+    },
 };
 
 export default nextConfig;
