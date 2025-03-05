0ba0e81754a5ed90fbc4c93e7eec6920af45126b
Yuberley <yuberleyguerrero1999@gmail.com>
2025-03-05 00:11:11 -0500

    test NextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: '**',
-            port: '',
-            pathname: '/**',
-          },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ycccqacoxogaexuxrjkm.supabase.co",
+        pathname: "/storage/v1/object/public/images/**",
+      },
+    ],
+  },  
 };
 
 export default nextConfig;
