b629f7b692d2794c1bfcc7db8500d04fb3861625
Yuberley <yuberleyguerrero1999@gmail.com>
2025-06-10 14:33:06 -0500

    change phone number

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,4 +4,4 @@ export const NAME_BUCKET_IMAGES = 'images';
 
 export const BASE_URL_THIS_WEBSITE = 'https://ylsport.store';
 
-export const PHONE_NUMBER = '573134571148';
+export const PHONE_NUMBER = '573125612902';
